273467364b59492d73c45284ec3c537b5f862b06
Captain Price <murathoncu@gmail.com>
2022-12-03 03:08:49 +0300

    fixed some issues

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import CardsInfo, { ButtonCard, CardMain } from "../styles/Card.styled";
 
@@ -6,8 +8,8 @@ const Cards = ({ info, searchName, searchType }) => {
 
   return (
     <>
-      {(searchType === "brawlers" || searchType === "gamemodes") && (
-        <CardMain wrap="wrap">
+      {searchType === "brawlers" && (
+        <CardMain wrap="wrap" style={{ overflowY: "hidden" }}>
           {info
             ?.filter((x) =>
               x?.name.toLowerCase().includes(searchName.toLowerCase())
@@ -37,6 +39,39 @@ const Cards = ({ info, searchName, searchType }) => {
             })}
         </CardMain>
       )}
+
+      {searchType === "gamemodes" && (
+        <CardMain wrap="wrap" style={{ overflowY: "scroll" }}>
+          {info
+            ?.filter((x) =>
+              x?.name.toLowerCase().includes(searchName.toLowerCase())
+            )
+            .map((item, index) => {
+              return (
+                <CardsInfo
+                  key={index}
+                  justify="baseline"
+                  style={{ backgroundColor: `${item?.bgColor}` }}>
+                  <h2>{item.name}</h2>
+                  <img
+                    src={item.imageUrl}
+                    alt=""
+                    width="150px"
+                    style={{ marginBottom: "1rem" }}
+                  />
+                  <p>{item.description}</p>
+                  {searchType === "brawlers" && (
+                    <ButtonCard
+                      onClick={() => navigate("detail", { state: item })}>
+                      View Details
+                    </ButtonCard>
+                  )}
+                </CardsInfo>
+              );
+            })}
+        </CardMain>
+      )}
+
       {searchType === "maps" && (
         <CardMain wrap="wrap">
           {info
@@ -48,8 +83,12 @@ const Cards = ({ info, searchName, searchType }) => {
                 <CardsInfo
                   key={index}
                   justify="baseline"
-                  style={{ backgroundColor: `${item?.gameMode?.color}` }}>
+                  style={{
+                    backgroundColor: `${item?.gameMode?.color}`,
+                    gap: "0.75rem",
+                  }}>
                   <h2 style={{ textAlign: "center" }}>{item?.name}</h2>
+                  <img src={item?.gameMode?.imageUrl} alt="" width="50px" />
                   <p style={{ textAlign: "center" }}>
                     -{item?.gameMode?.name}-
                   </p>
@@ -63,24 +102,6 @@ const Cards = ({ info, searchName, searchType }) => {
                 </CardsInfo>
               );
             })}
-        </CardMain>
-      )}
-      {searchType === "icons" && (
-        <CardMain wrap="wrap">
-          {info?.map((item, index) => {
-            return (
-              <CardsInfo key={index} justify="baseline">
-                <h2>{item?.name}</h2>
-                <img src={item?.imageUrl} alt="" width="150px" />
-                <p>{item?.description}</p>
-
-                <ButtonCard
-                  onClick={() => (window.location.href = `${item?.link}`)}>
-                  View Details
-                </ButtonCard>
-              </CardsInfo>
-            );
-          })}
         </CardMain>
       )}
     </>
