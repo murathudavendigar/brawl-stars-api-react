dd22776c74e8053ba43fc7993b30811876224e0a
Captain Price <murathoncu@gmail.com>
2022-12-03 03:10:09 +0300

    add new font style

--- src/styles/GlobalStyles.jsx
+++ src/styles/GlobalStyles.jsx
@@ -1,7 +1,7 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
- @import url('https://fonts.cdnfonts.com/css/nougat');
+@import url('https://fonts.googleapis.com/css2?family=Lilita+One&display=swap');
 *{
       margin:0;
     padding: 0;
@@ -14,8 +14,8 @@ body{
     background-image: url("https://preview.redd.it/iapqmzd3zu721.jpg?auto=webp&s=d70e6a1f8d6a9b4cfc8bb210d3438ce9a0478169");
     background-size: contain;
     background-repeat: repeat;
-     font-family: 'Nougat', sans-serif;
-     height: 100vh;
+    font-family: 'Lilita One', cursive;
+    height: 100vh;
 
     
     }
